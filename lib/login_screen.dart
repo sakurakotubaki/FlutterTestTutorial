03f76fc72ee11101ebab592d7dff82b62fd2afb9
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2022-12-24 21:43:44 +0900

    update VvalidatetionTest

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/validator.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,8 +9,44 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _key,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                key: const ValueKey("email_id"),
+                decoration: const InputDecoration(hintText: "Enter Email Id"),
+                validator: (value) => Validator.validateEmail(value ?? ""),
+              ),
+              TextFormField(
+                controller: _passwordController,
+                key: const ValueKey("password"),
+                decoration: const InputDecoration(hintText: "Enter Password"),
+                validator: (value) => Validator.validatePassword(value ?? ""),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                  onPressed: () {
+                    _key.currentState?.validate();
+                  },
+                  child: Text("Login"))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
